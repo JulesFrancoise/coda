b7067124835ba7495680cbf045c35c93c69f2cd6
Jules Françoise <me@julesfrancoise.com>
2018-11-23 14:23:46 +0100

    Fix fromMax source to allow multiple simultaneous channels

--- packages/max/src/frommax.js
+++ packages/max/src/frommax.js
@@ -1,33 +1,77 @@
 import { parseParameters } from '@coda/prelude';
 import { currentTime } from '@most/scheduler';
 
-let wsocket;
-let connected = false;
+class SocketClient {
+  constructor() {
+    this.connected = false;
+    this.socket = null;
+    this.channels = {};
+  }
 
-function connectWebsocket(host, port) {
-  if (!('WebSocket' in window)) throw new Error('Websockets are not supported');
-  if (connected) return;
-  const address = `ws://${host}:${port}/`;
-  // eslint-disable-next-line no-console
-  console.log('::FROMMAX:: Connecting to', address);
-  wsocket = new WebSocket(address);
-  wsocket.onopen = () => {
+  connect(host, port) {
+    if (!('WebSocket' in window)) throw new Error('Websockets are not supported');
+    if (this.connected) return;
+    const address = `ws://${host}:${port}/`;
     // eslint-disable-next-line no-console
-    console.log(`::FROMMAX:: CONNECTED to ${address}`);
-    connected = true;
-  };
+    console.log('::FROMMAX:: Connecting to', address);
+    this.socket = new WebSocket(address);
+    this.socket.onopen = () => {
+      // eslint-disable-next-line no-console
+      console.log(`::FROMMAX:: CONNECTED to ${address}`);
+      this.connected = true;
+    };
 
-  wsocket.onclose = () => {
-    // eslint-disable-next-line no-console
-    console.log(`::FROMMAX:: DISCONNECTED from ${address}`);
-    connected = false;
-  };
+    this.socket.onclose = () => {
+      // eslint-disable-next-line no-console
+      console.log(`::FROMMAX:: DISCONNECTED from ${address}`);
+      this.connected = false;
+    };
 
-  wsocket.onerror = (e) => {
-    // eslint-disable-next-line no-console
-    console.log('::TOMAX:: WebSocket error at address', address, '\n', e);
-  };
+    this.socket.onerror = (e) => {
+      // eslint-disable-next-line no-console
+      console.log('::TOMAX:: WebSocket error at address', address, '\n', e);
+    };
+  }
+
+  addChannel(name, callback) {
+    if (Object.keys(this.channels).includes(name)) {
+      console.log('Channel', name, 'already exists');
+      return;
+    }
+    this.channels[name] = callback;
+    this.update();
+  }
+
+  removeChannel(name) {
+    if (!Object.keys(this.channels).includes(name)) {
+      console.log('Channel', name, 'does not exists');
+      return;
+    }
+    delete this.channels[name];
+    this.update();
+  }
+
+  update() {
+    this.socket.onmessage = (ev) => {
+      // was it a dict?
+      if (ev.data.charAt(0) === '{') {
+        console.log('Dicts are not currently supported');
+        // attempt to json parse it:
+        // const tree = JSON.parse(ev.data);
+        // console.log(`parsed ${JSON.stringify(tree)}`);
+      } else {
+        const [ch, ...data] = ev.data.substr(0, 50).split(' ');
+        Object.keys(this.channels).forEach((channel) => {
+          if (ch === channel) {
+            this.channels[channel](data);
+          }
+        });
+      }
+    };
+  }
 }
+
+const socketClient = new SocketClient();
 
 /**
  * Parameter definitions
@@ -70,26 +114,18 @@ function tryEvent(t, x, sink) {
  */
 export default function fromMax(options = {}) {
   const { hostname, channel, size } = parseParameters(definitions, options);
-  connectWebsocket(hostname, 8081);
+  // connectWebsocket(hostname, 8081);
+  socketClient.connect(hostname, 8081);
   return {
     attr: { format: size > 1 ? 'vector' : 'scalar', size },
     run(sink, scheduler) {
-      wsocket.onmessage = (ev) => {
-        // was it a dict?
-        if (ev.data.charAt(0) === '{') {
-          console.log('Dicts are not currently supported');
-          // attempt to json parse it:
-          // const tree = JSON.parse(ev.data);
-          // console.log(`parsed ${JSON.stringify(tree)}`);
-        } else {
-          const [ch, ...data] = ev.data.substr(0, 50).split(' ');
-          if (ch === channel) {
-            tryEvent(currentTime(scheduler), data.map(parseFloat), sink);
-          }
-        }
-      };
+      socketClient.addChannel(channel, (data) => {
+        tryEvent(currentTime(scheduler), data.map(parseFloat), sink);
+      });
       return {
-        dispose: () => {},
+        dispose: () => {
+          socketClient.removeChannel(channel);
+        },
       };
     },
   };
